4ad7633a9699448732596678c2cc9001b0e79382
tradichel <teri.radichel@watchguard.com>
2017-05-24 11:04:15 -0700

    delete files in S3 log bucket

--- code/execute/delete_files.sh
+++ code/execute/delete_files.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 bucket=$(./execute/get_output_value.sh "firebox-cli-s3bucket" "FireboxPrivateBucket")
+logbucket=$(./execute/get_output_value.sh "firebox-cli-s3bucket" "FireboxLogBucket")
 
 if [ "$bucket" == "" ]; then
     echo "bucket name not found"
@@ -10,5 +11,12 @@ echo "delete files from $bucket"
 aws s3 rm --recursive s3://$bucket > delfiles.txt  2>&1
 error=$(cat delfiles.txt | grep "error\|Unknown\|path")
 if [ "$error" != "" ]; then
-    echo "Error deleting files from bucket: $error"
+    echo "Error deleting files from $bucket: $error"
+fi
+
+echo "delete files from $logbucket"
+aws s3 rm --recursive s3://$logbucket > delfiles.txt  2>&1
+error=$(cat delfiles.txt | grep "error\|Unknown\|path")
+if [ "$error" != "" ]; then
+    echo "Error deleting files from $logbucket: $error"
 fi
